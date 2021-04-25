2475411ea5d2a6fdc0c633ff57620ef3ae0207f1
Sonicspeedfly <sonicspeedfly@gmail.com>
2021-04-25 22:19:49 +0500

    v1.6.0

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Sonicspeedfly/wallet/v1.1.0/pkg/types"
 	"github.com/Sonicspeedfly/wallet/v1.1.0/pkg/wallet"
 )
 func main() {
 	svc := wallet.Service{}
-	file := svc.Import("./information")
-	fmt.Print(file)
+	payments := []types.Payment{
+		{
+			ID: "1",
+			AccountID: 1,
+			Amount: 10000,
+			Category: "a",
+			Status: "INPROGRESS",
+		},
+		{
+			ID: "1",
+			AccountID: 1,
+			Amount: 10000,
+			Category: "a",
+			Status: "INPROGRESS",
+		},
+	}
+
+
+	file := svc.HistoryToFiles(payments, "./information", 1)
+	fmt.Print(file, payments)
 
 }
